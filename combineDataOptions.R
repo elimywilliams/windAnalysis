253a61b5f1f44aa6f0925e502b585f1fffa680a6
elimywilliams <62444225+elimywilliams@users.noreply.github.com>
2020-11-20 10:28:18 -0700

    Update combineDataOptions.R

--- combineDataOptions.R
+++ combineDataOptions.R
@@ -2,9 +2,9 @@ fileFolder <- '/Users/emilywilliams/Downloads/archive_201001_101207_driving'
 AerisFileName <- 'Aeris.csv'
 LocationFileName <- "Location.csv"
 WindFileName <- 'WindData.csv'
+headingFileName <- 'VesselHeading.csv'
 finalFolder <- '/Users/emilywilliams/Downloads/archive_201001_101207_driving'
 finalFilename <- 'finalAggregated.csv'
 combinedScriptLoc <- '/Users/emilywilliams/Documents/windAnalysis/combineDataScript.R'
 
 suppressMessages(source(combinedScriptLoc))
-
